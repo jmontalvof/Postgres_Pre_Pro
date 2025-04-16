ab2dab8dda75d403d1acd5741baed8e2d3edb9e6
jmontalvof <jmontalvof@outlook.es>
2025-04-16 16:09:10 +0200

    nuevo deploy

--- src/inserta_usuarios.sql
+++ src/inserta_usuarios.sql
@@ -5,11 +5,4 @@ CREATE TABLE IF NOT EXISTS :"schema".usuarios_demo (
 );
 
 INSERT INTO :"schema".usuarios_demo (nombre, email)
-VALUES ('Jorge', 'jorge@inicial.com')CREATE TABLE IF NOT EXISTS :"schema".usuarios_demo (
-  id SERIAL PRIMARY KEY,
-  nombre TEXT,
-  email TEXT
-);
-
-INSERT INTO :"schema".usuarios_demo (nombre, email)
 VALUES ('Jorge', 'jorge@inicial.com');
