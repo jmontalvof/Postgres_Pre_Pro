d71c71c450b3d5548ae4c052599c31a10445c28c
jmontalvof <jmontalvof@outlook.es>
2025-04-16 21:48:07 +0200

    ✅ Corregidos scripts con \set schema para evitar errores

--- src/actualiza_usuarios.sql
+++ src/actualiza_usuarios.sql
@@ -1,12 +1,13 @@
+\set schema 'public'
+
 CREATE TABLE IF NOT EXISTS :"schema".usuarios_demo (
-    id SERIAL PRIMARY KEY,
-    nombre TEXT,
-    email TEXT
+  id SERIAL PRIMARY KEY,
+  nombre TEXT,
+  email TEXT
 );
 
 UPDATE :"schema".usuarios_demo
 SET email = 'jorge@actualizado.com'
 WHERE nombre = 'Jorge';
 
-SELECT * FROM :"schema".usuarios_demo
-WHERE nombre = 'Jorge';
+SELECT * FROM :"schema".usuarios_demo WHERE nombre = 'Jorge';
