dfc63c01b068f4396cadca293e21e5592893d0aa
jmontalvof <jmontalvof@outlook.es>
2025-04-18 01:32:45 +0200

    🧪 Añadir script con errores para test de autocorrección

---
+++ src/script_con_errores.sql
@@ -0,0 +1,8 @@
+-- Script intencionalmente mal para probar la autocorrección con IA
+CREAT TABLE demo_usuarios (
+  id SERIAL PRIMAR KEY,
+  nombre TEXT
+;
+
+INSERT INTO demo_usuarios (nombre)
+VALUES ('Prueba Jorge')
