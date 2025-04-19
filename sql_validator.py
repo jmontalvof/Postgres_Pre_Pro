172e2eab1374037cbb0f569f4d2ce9308e6efdaf
jmontalvof <jmontalvof@outlook.es>
2025-04-19 18:51:49 +0200

    DESPLIEGUE01

--- sql_validator.py
+++ sql_validator.py
@@ -1,14 +1,14 @@
 from sqlfluff.core import Linter
 
 def validate_sql(path):
-    linter = Linter(dialect="postgres")
-    result = linter.lint_path(path)
+   linter = Linter(dialect="postgres")
+   result = linter.lint_path(path)
 
-    errors = []
-    for v in result.get_violations():
-        errors.append({"line": v.line_no, "message": v.desc()})
+   errors = []
+   for v in result.get_violations():
+      errors.append({"line": v.line_no, "message": v.desc()})
 
-    with open(path, "r") as f:
-        lines = f.readlines()
+   with open(path, "r") as f:
+      lines = f.readlines()
 
-    return len(errors) == 0, errors, len(lines)
+   return len(errors) == 0, errors, len(lines)
