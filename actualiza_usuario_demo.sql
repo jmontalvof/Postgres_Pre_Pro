d3808ee0e61ac6507eb0ae195849c09a8ca50e37
jmontalvof <jmontalvof@outlook.es>
2025-04-20 02:22:21 +0200

    Desplegando

---
+++ actualiza_usuario_demo.sql
@@ -0,0 +1,4 @@
+-- actualiza_usuario_demo.sql
+UPDATE usuarios_demo
+SET nombre = 'Jorge Actualizado', email = 'jorge@actualizado.com'
+WHERE id = 1;
---
+++ inserta_usuario_demo.sql
@@ -0,0 +1,3 @@
+-- inserta_usuario_demo.sql
+INSERT INTO usuarios_demo (nombre, email)
+VALUES ('Nuevo Usuario', 'nuevo@correo.com');
