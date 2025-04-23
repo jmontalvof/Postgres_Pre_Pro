b83622bd8c730b9cd700f12011847858671ff22c
jmontalvof <jmontalvof@outlook.es>
2025-04-23 17:26:44 +0200

    Probando los entornos

--- src/borra_usuario.sql
+++ src/borra_usuario.sql
@@ -1,6 +1,6 @@
--- Borra usuarios llamados 'Jorge' si existen en la tabla usuarios_demo
-DELETE FROM :"schema".usuarios_demo
+-- Borra el usuario llamado 'Jorge'
+DELETE FROM public.usuarios_demo
 WHERE nombre = 'Jorge';
 
 -- Muestra la tabla después de eliminar
-SELECT * FROM :"schema".usuarios_demo;
+SELECT * FROM public.usuarios_demo;
